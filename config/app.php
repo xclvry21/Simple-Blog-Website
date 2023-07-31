4b0f511bd5350c7d1e40efd33d8a27f20c44ccc3
xcl21 <axcle.viray21@gmail.com>
2023-07-31 16:57:47 +0800

    install laravel jetstream for the authentication
    - add timezone env
    - change tab icon

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE'),
 
     /*
     |--------------------------------------------------------------------------
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
     ])->toArray(),
 
     /*
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('images') }}/blog.png" alt="logo-dark" height="10">
