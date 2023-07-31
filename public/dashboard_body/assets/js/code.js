443fb61b24df34814368354460830ce4bbb968e7
xcl21 <axcle.viray21@gmail.com>
2023-07-31 18:20:34 +0800

    import public assets for user dashboard

---
+++ public/dashboard_body/assets/js/code.js
@@ -0,0 +1,41 @@
+//sweet alert delete function
+$(function() {
+    $(document).on('click', '#delete', function(e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Delete This Data?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = link
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
+    });
+
+    // customized swals
+    $(document).on('click', '#swal-restore', function(e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+        window.location.href = link
+        Swal.fire(
+            'Success',
+            'Note restored successfully',
+            'success'
+        )
+    });
+
+});
